f754b06edd766bf263343d35edbd4a73536967e1
838021263 <88585395+838021263@users.noreply.github.com>
2021-08-15 21:20:44 +0800

    在线更新

--- HelloGit/src/main/java/com/atguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -8,6 +8,7 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git1");
         System.out.println("hello git2");
-        System.out.println("hello git3");
+        System.out.println("hello git3");      
+        System.out.println("在线更新");
     }
 }
